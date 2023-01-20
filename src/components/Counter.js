5585989143019e427bb5aaf096253e8f8c199840
Isabel <isamdrituerto@hotmail.com>
2023-01-20 09:20:38 +0100

    Added files to component and containers folders + imported Counters to MyAppContainers

---
+++ src/components/Counter.js
@@ -0,0 +1,15 @@
+
+const Counter = () => {
+    return(
+        <>
+            <h2>Cake x</h2>
+            <p>Ingredients: coming soon</p>
+            <p>Price: coming soon</p>
+            <p>Rating: coming soon</p>
+            <button>Add</button>
+            <button>Remove</button>
+    </>
+    )
+}
+
+export default Counter;
---
+++ src/containers/MyAppContainer.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Counter from "../components/Counter";
+
+//function
+const MyAppContainer = () => {
+
+//methods
+
+//components
+return (
+    <>
+    <h2>Cakes</h2>
+    <h3>Average rating: Coming soon</h3>
+    <Counter title="Victoria sponge"/>
+    <Counter title="Tea loaf"/>
+    <Counter title="Carrot cake"/>
+    <h3>Cakes sold: Coming soon</h3>
+    </>
+)
+
+}
+
+export default MyAppContainer;
