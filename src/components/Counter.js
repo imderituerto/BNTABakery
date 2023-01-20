b519b75c3aac86d0bfbb15052aa42b183ef5fb7b
Isabel <isamdrituerto@hotmail.com>
2023-01-20 09:32:28 +0100

    Added state to MyAppContainer.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,23 @@
 
-const Counter = () => {
+const Counter = ({title}, {ingredients}, {price}, {rating}, {onButtonClick}) => {
+
+    //add cake
+    const handleAddCake = () => {
+        
+    }
+
+    //remove cake
+    const handleRemoveCake = () => {
+        
+    }
+
+    //return counters
     return(
         <>
-            <h2>Cake x</h2>
-            <p>Ingredients: coming soon</p>
-            <p>Price: coming soon</p>
-            <p>Rating: coming soon</p>
+            <h2>Cake {title}</h2>
+            <p>Ingredients: {ingredients}</p>
+            <p>Price: {price}</p>
+            <p>Rating: {rating}</p>
             <button>Add</button>
             <button>Remove</button>
     </>
--- src/containers/MyAppContainer.js
+++ src/containers/MyAppContainer.js
@@ -4,19 +4,30 @@ import Counter from "../components/Counter";
 //function
 const MyAppContainer = () => {
 
-//methods
+    //states
+    // victoriaCounter () = useState(0)
+    // teaCounter () = useState(0)
+    // carrotCounter () = useState(0)
+    
+    const [victoriaCounter, setVictoriaCounter] = useState(0);
+    const [teaCounter, setTeaCounter] = useState(0);
+    const [carrotCounter, setCarrotCounter] = useState(0);
+    
+    const incrementCounter = () => {
+        setCounter(counter + 1);
+    }
 
-//components
-return (
-    <>
-    <h2>Cakes</h2>
-    <h3>Average rating: Coming soon</h3>
-    <Counter title="Victoria sponge"/>
-    <Counter title="Tea loaf"/>
-    <Counter title="Carrot cake"/>
-    <h3>Cakes sold: Coming soon</h3>
-    </>
-)
+    //components
+    return (
+        <>
+        <h2>Cakes</h2>
+        <h3>Average rating: Coming soon</h3>
+        <Counter title="Victoria sponge" onButtonClick={victoriaCounter}/>
+        <Counter title="Tea loaf" onButtonClick={teaCounter}/>
+        <Counter title="Carrot cake" onButtonClick={carrotCounter}/>
+        <h3>Cakes sold: Coming soon</h3>
+        </>
+    )
 
 }
 
